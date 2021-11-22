543c7e5ec7c4ce721c95ad7884f75be409b9cb41
Your Name Here <mhoobs@umich.edu>
2021-11-21 19:39:28 -0500

    Update marlee.py
    Set up data base for one fruit so far (apple)

--- marlee.py
+++ marlee.py
@@ -1,3 +1,4 @@
+from logging import fatal
 import unittest
 import sqlite3
 import json
@@ -11,21 +12,9 @@ import urllib3
 urllib3.disable_warnings()
 
 
-# def getFruitInfo(fruit):
-#     url = 'https://www.fruityvice.com/api/fruit/'
-#     request = requests.get(url + fruit, verify = False)
-#     data = request.json()
-#     print(data)
-#     nutrition_dict = {}
-#     for nut_val in data:
-#         nutrition_dict[nut_val] = data[nut_val]
-#     return nutrition_dict
-
-# getFruitInfo('apple')
-
 # def getFruitInfo():
 #     url = 'https://www.fruityvice.com/api/fruit/'
-#     lst_fruits = ['apple', 'apricot', 'banana', 'blueberry']
+#     lst_fruits = ['apple', 'apricot', 'banana', 'blueberry', 'cherry', 'grapes', 'guava', 'lemon', 'lime', 'mango', 'melon', 'orange', 'papaya', 'pear', 'persimmon']
 #     fruit_dict = {}
 #     for fruit in lst_fruits:
 #         request = requests.get(url + fruit, verify = False)
@@ -33,27 +22,59 @@ urllib3.disable_warnings()
 #         nutrition_dict = {}
 #         for nut_val in data:
 #             if nut_val == 'nutritions':
-#                 nutrition_dict[nut_val] = data[nut_val]
+#                 for nut in data[nut_val]:
+#                     nutrition_dict[nut] = data[nut_val][nut]
 #         fruit_dict[fruit] = nutrition_dict
 #     return fruit_dict
 
-# print(getFruitInfo())
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
 
-def getFruitInfo():
+# def getFruitInfo():
+#     url = 'https://www.fruityvice.com/api/fruit/'
+#     lst_fruits = ['apple', 'apricot', 'banana', 'blueberry', 'cherry', 'grapes', 'guava', 'lemon', 'lime', 'mango', 'melon', 'orange', 'papaya', 'pear', 'persimmon']
+#     fruit_dict = {}
+#     for fruit in lst_fruits:
+#         request = requests.get(url + fruit, verify = False)
+#         data = request.json()
+#         nutrition_dict = {}
+#         for nut_val in data:
+#             nutrition_dict[nut_val] = data[nut_val]
+#         fruit_dict[fruit] = nutrition_dict
+#     return fruit_dict
+
+def getFruitInfo(fruit):
     url = 'https://www.fruityvice.com/api/fruit/'
-    lst_fruits = ['apple', 'apricot', 'banana', 'blueberry', 'cherry', 'grapes', 'guava', 'lemon', 'lime', 'mango', 'melon', 'orange', 'papaya', 'pear', 'persimmon']
-    fruit_dict = {}
-    for fruit in lst_fruits:
-        request = requests.get(url + fruit, verify = False)
-        data = request.json()
-        nutrition_dict = {}
-        for nut_val in data:
-            if nut_val == 'nutritions':
-                for nut in data[nut_val]:
-                    nutrition_dict[nut] = data[nut_val][nut]
-        fruit_dict[fruit] = nutrition_dict
-    return fruit_dict
+    request = requests.get(url + fruit, verify = False)
+    data = request.json()
+    nutrition_dict = {}
+    for nut_val in data:
+        nutrition_dict[nut_val] = data[nut_val]
+    return nutrition_dict
 
-print(getFruitInfo())
+def create_apple_table(cur, conn):
+    data = getFruitInfo('apple')
+    cur.execute("CREATE TABLE IF NOT EXISTS Apple (id INTEGER PRIMARY KEY, genus TEXT, name TEXT, family TEXT, ord TEXT, carbs INTEGER, pro INTEGER, fat INTEGER, cals INTEGER, sug INTEGER)")
+    id = data['id']
+    genus = data['genus']
+    name = data['name']
+    family = data['family']
+    ord = data['order']
+    carbs = data['nutritions']['carbohydrates']  
+    pro = data['nutritions']['protein']
+    fat = data['nutritions']['fat']
+    cals = data['nutritions']['calories']
+    sug = data['nutritions']['sugar']
+    cur.execute("INSERT INTO Apple (id, genus, name, family, ord, carbs, pro, fat, cals, sug) VALUES (?,?,?,?,?,?,?,?,?,?)", (id, genus, name, family, ord, carbs, pro, fat, cals, sug))
+    conn.commit()
 
-
+def main():
+    # SETUP DATABASE AND TABLE
+    cur, conn = setUpDatabase('fruity_vice.db')
+    create_apple_table(cur, conn)
+    
+if __name__ == "__main__":
+    main()
